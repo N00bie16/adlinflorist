f7a184acc6be16e28e713dc4d2241e416a069e4c
N00bie16 <reynaldir16@gmail.com>
2024-01-23 10:05:26 +0700

    feat:webhook

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,63 +1,39 @@
-// route.ts
-import { headers } from "next/headers";
 import { NextResponse } from "next/server";
-
-import midtransClient from "midtrans-client"; // Tambahkan ini
+import { sha512 } from "js-sha512";
 import prismadb from "@/lib/prismadb";
 
 export async function POST(req: Request) {
-  const body = await req.text();
-  const signature = headers().get("X-Midtrans-Signature") as string; // Ubah nama header sesuai dengan Midtrans
-
-  const snap = new midtransClient.Snap({
-    isProduction: false,
-    serverKey: process.env.MIDTRANS_SERVER_KEY!,
-    clientKey: process.env.MIDTRANS_CLIENT_KEY!,
-  });
-
-  // let event: midtransClient.WebhookEvent;
-
   try {
-    event = snap.webhook.constructEvent(body, signature);
-  } catch (error: any) {
-    return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 });
+    const data = await req.json();
+    const serverKey = process.env.MIDTRANS_SERVER_KEY;
+    const {
+      order_id,
+      status_code,
+      gross_amount,
+      signature_key,
+      transaction_status,
+    } = data;
+
+    const hashed = sha512(order_id + status_code + gross_amount + serverKey);
+    if (hashed == signature_key) {
+      if (transaction_status == "settlement") {
+        const order = await prismadb.order.update({
+          where: {
+            id: order_id,
+          },
+          data: {
+            isPaid: true,
+          },
+          include: {
+            orderItems: true,
+          },
+        });
+        console.log(order);
+      }
+    }
+
+    return NextResponse.json({ data: data }, { status: 201 });
+  } catch (error) {
+    NextResponse.json({ error: error }, { status: 500 });
   }
-
-  // const transaction = event.transaction_status;
-  // const orderId = event.order_id;
-
-  // if (transaction === "capture") {
-  //   // Pembayaran berhasil
-  //   const order = await prismadb.order.update({
-  //     where: {
-  //       id: orderId,
-  //     },
-  //     data: {
-  //       isPaid: true,
-  //       // Tambahkan data lain yang ingin Anda perbarui
-  //     },
-  //     include: {
-  //       orderItems: true,
-  //     },
-  //   });
-
-  // const productIds = order.orderItems.map((orderItem) => orderItem.productId);
-
-  //   await prismadb.product.updateMany({
-  //     where: {
-  //       id: {
-  //         in: [...productIds],
-  //       },
-  //     },
-  //     data: {
-  //       isArchived: true,
-  //     },
-  //   });
-  // } else if (transaction === "cancel" || transaction === "deny") {
-  //   return new NextResponse(`Pembayaran dibatalkan: ${event.cancel_reason}`, {
-  //     status: 400,
-  //   });
-  // }
-
-  return new NextResponse(null, { status: 200 });
 }
